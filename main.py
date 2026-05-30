f58998d2065fee8b442f32688ac9af3dc46d403a
ryxi-lgtm <ryxi@ucdavis.edu>
2026-05-30 06:11:32 -0700

    begin OPP refactor: platforms

--- main.py
+++ main.py
@@ -37,53 +37,50 @@ bg_star=[bg_star1,bg_star2]
 bg_star_index=0
 bg_timer=0
 
-def create_platform(x):
-    width=random.randint(350,650)
-    y=random.randint(430, 620)
-    height=720-y
-    gd_surf=pygame.Surface((width,height))
-    gd_surf.fill('BLACK')
-    gd_rect=gd_surf.get_rect(topleft=(x,y))
-    return {
-        "surf":gd_surf,
-        "rect":gd_rect
-    }
+class Platform:
+    def __init__(self, x, upper=False):
+        if upper:
+            width = random.randint(320, 600)
+            y = random.randint(120, 280)
+            color = "GRAY"
+        else:
+            width = random.randint(500, 850)
+            y = random.randint(430, 620)
+            color = "BLACK"
 
-def create_platform2(x): #upper-level platforms
-    width=random.randint(280,520)
-    y=random.randint(120, 280)
-    height=720-y
-    gd_surf=pygame.Surface((width,height))
-    gd_surf.fill('GRAY')
-    gd_rect=gd_surf.get_rect(topleft=(x,y))
-    return {
-        "surf":gd_surf,
-        "rect":gd_rect
-    }
+        height = 720 - y
+        self.surf = pygame.Surface((width, height))
+        self.surf.fill(color)
+        self.rect = self.surf.get_rect(topleft=(x, y))
+        self.upper = upper
 
+    def update(self, speed):
+        if self.upper:
+            self.rect.x -= speed * 0.9
+        else:
+            self.rect.x -= speed
 
-
+    def draw(self, surface):
+        surface.blit(self.surf, self.rect)
 platforms=[]
 platforms2=[]
 
-start_surf = pygame.Surface((900, 120)) #initial platform
-start_surf.fill("BLACK")
-start_rect = start_surf.get_rect(topleft=(0, 600))
-platforms.append({
-    "surf": start_surf,
-    "rect": start_rect
-})
+start_platform = Platform(0)
+start_platform.surf = pygame.Surface((900, 120))
+start_platform.surf.fill("BLACK")
+start_platform.rect = start_platform.surf.get_rect(topleft=(0, 600))
+platforms.append(start_platform)
 
-x=start_rect.right+150
+x=start_platform.rect.right+150
 for i in range(5):
-    platform=create_platform(x)
+    platform = Platform(x)
     platforms.append(platform)
-    gap=random.randint(120,320)
-    x=platform["rect"].right+gap
+    gap = random.randint(150, 300)
+    x = platform.rect.right + gap
 
 player=pygame.Surface((player_w,player_h)) #Sparkle
 player.fill('RED')
-player_rect=player.get_rect(midbottom=(player_x,start_rect.top))
+player_rect = player.get_rect(midbottom=(player_x, start_platform.rect.top))
 
 vel_y=0
 gravity=0.8
@@ -103,6 +100,41 @@ target_x=player_x
 slide_timer=0
 slide_duration=15
 fast_land=False
+
+def draw_game():
+    game_surface.blit(bg_star[bg_star_index], (0, 0))
+
+    for platform2 in platforms2:
+        platform2.draw(game_surface)
+    for platform in platforms:
+        platform.draw(game_surface)
+
+    game_surface.blit(player, player_rect)
+
+    display_distance = int(distance * 0.08)
+
+    distance_text = font.render(
+        f"DIST {display_distance}",
+        False,
+        (220, 220, 230)
+    )
+
+    speed_text = font.render(
+        f"SPD {platform_speed:.1f}",
+        False,
+        (220, 220, 230)
+    )
+
+    game_surface.blit(distance_text, (30, 20))
+    game_surface.blit(speed_text, (30, 55))
+
+    scaled_surface = pygame.transform.scale(
+        game_surface,
+        (DISPLAY_W, DISPLAY_H)
+    )
+
+    screen.blit(scaled_surface, (0, 0))
+    pygame.display.update()
 
 while True:
     for event in pygame.event.get():
@@ -144,10 +176,10 @@ while True:
     if distance >3000 and len(platforms2)==0: #creat upper-level platforms
         x2=1400
         for i in range(5):
-            platform2=create_platform2(x2)
+            platform2 = Platform(x2, upper=True)
             platforms2.append(platform2)
-            gap2=random.randint(120,320)
-            x2=platform2["rect"].right+gap2
+            gap2 = random.randint(260, 500)
+            x2 = platform2.rect.right + gap2
         
     if distance>=speed_up_every:
         speed_goal+=0.2
@@ -158,8 +190,8 @@ while True:
         platform_speed+=0.01 #smoothing the process of speeding up
 
     for platform in platforms:
-        rect=platform["rect"]
-        rect.x-=platform_speed
+        platform.update(platform_speed)
+        rect = platform.rect
         if player_rect.colliderect(rect):
             if old_bottom<= rect.top and vel_y>=0:
                 player_rect.bottom=rect.top
@@ -173,19 +205,19 @@ while True:
             else:
                 player_rect.right=rect.left
                 touching_side=True
-    platforms=[platform for platform in platforms if platform["rect"].right>0]
+    platforms = [platform for platform in platforms if platform.rect.right > 0]
     #removing platforms that are off screen
 
-    last_platform=platforms[-1]
+    last_platform = platforms[-1]
 
-    if last_platform["rect"].right<1280:
-        gap=random.randint(120,320)
-        new_x=last_platform["rect"].right+gap
-        platforms.append(create_platform(new_x))
+    if last_platform.rect.right < 1280:
+        gap=random.randint(200,320)
+        new_x = last_platform.rect.right + gap
+        platforms.append(Platform(new_x))
 
     for platform2 in platforms2:
-        rect=platform2["rect"]
-        rect.x -= platform_speed * 0.9
+        platform2.update(platform_speed)
+        rect = platform2.rect
         if player_rect.colliderect(rect):
             if old_bottom<=rect.top and vel_y>=0:
                 player_rect.bottom=rect.top
@@ -196,14 +228,14 @@ while True:
                     slide_timer=int(slide_duration/1.5)
                     target_x=450
                     fast_land=False
-    platforms2=[platform2 for platform2 in platforms2 if platform2["rect"].right>0]
+    platforms2 = [platform2 for platform2 in platforms2 if platform2.rect.right > 0]
     
     if len(platforms2)>0:
         last_platform2 = platforms2[-1]
-        if last_platform2["rect"].right<1280:
+        if last_platform2.rect.right < 1280:
             gap2=random.randint(260,500)
-            new_x2=last_platform2["rect"].right+gap2
-            platforms2.append(create_platform2(new_x2))
+            new_x2 = last_platform2.rect.right + gap2
+            platforms2.append(Platform(new_x2, upper=True))
 
     if slide_timer>0:
         slide_timer-=1
@@ -227,34 +259,5 @@ while True:
         pygame.quit()
         sys.exit()
 
-    game_surface.blit(bg_star[bg_star_index],(0,0))
-
-    for platform2 in platforms2:
-        game_surface.blit(platform2["surf"], platform2["rect"])
-    for platform in platforms:
-        game_surface.blit(platform["surf"], platform["rect"])
-    
-    game_surface.blit(player,player_rect)
-
-    distance_text = font.render(
-        f"DIST {int(distance*0.08)}",
-        False,
-        (220,220,230)
-    )
-
-    speed_text = font.render(
-        f"SPD {platform_speed:.1f}",
-        False,
-        (220,220,230)
-    )
-    
-    game_surface.blit(distance_text, (30, 20))
-    game_surface.blit(speed_text, (30, 55))
-
-    scaled_surface = pygame.transform.scale(
-    game_surface,
-    (DISPLAY_W, DISPLAY_H)
-    )
-    screen.blit(scaled_surface, (0, 0))
-    pygame.display.update()
+    draw_game()
     clock.tick(60)
